dade152c7a9c2b7cdf7897edb99efd9e7b5669d0
chrisk8er <chrisk8er@gmail.com>
2021-12-16 11:36:21 +0800

    adding phone authentication by firebase

--- index.js
+++ index.js
@@ -20,7 +20,14 @@ const config = {
   serverURL: process.env.SERVER_URL || "http://localhost:1337/parse", // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  auth: {
+    firebase: {
+        module: 'parse-server-firebase-phone-auth',
+        serviceAccount: './e-commerce-id-firebase-adminsdk-1i2kh-2faa39ea97.json',
+        databaseURL: 'https://e-commerce-id.firebaseio.com'
+    }
+}
 };
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
